d8dfddf5d07e48d8cf5b9bcad5dc6e85d4aee4fa
CQuiroga <40526931+CQuiroga@users.noreply.github.com>
2018-08-22 01:43:04 -0500

    'Página en construcción' para 'practica' y 'aprende'

---
+++ app/controllers/calcula_controller.rb
@@ -0,0 +1,5 @@
+class CalculaController < ApplicationController
+  def index
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -14,12 +14,15 @@ Rails.application.routes.draw do
     resources :eventos
     resources :politica
     resources :terminos
+    resources :cultura
+    resources :aprende
+    resources :practica
+    resources :calcula
 
     get 'puntos_cercanos/puntos_cercanos'
     get 'favoritos/favoritos'
     get 'recientes/recientes'
     get 'beneficios/beneficios'
-    get 'cultura/cultura'
     get 'home/index'
     root 'home#index'
   end

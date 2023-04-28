4d426c9e5512a9d4ad9bae2e81c55d94e4be2edc
camilo quiroga <camilo.quirogam@gmail.com>
2023-04-28 18:38:35 -0500

    adición de dominio para deploy

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -5,9 +5,9 @@ Rails.application.configure do
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
-  
+
   # --------- despliegue a producción -------------
-  config.hosts << "eshop-production-f941.up.railway.app"
+  config.hosts << "limpiapp-web-production.up.railway.app"
 
   # Do not eager load code on boot.
   config.eager_load = false
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # --------- despliegue a producción -------------
-  config.hosts << "eshop-production-f941.up.railway.app"
+  config.hosts << "limpiapp-web-production.up.railway.app"
 
   # Code is not reloaded between requests.
   config.cache_classes = true
